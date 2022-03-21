b428496432695ae44a5907973a21ff3f9442e917
anujah999 <96858841+anujah999@users.noreply.github.com>
2022-03-21 19:40:49 +0530

    Update hey.sh

--- hey.sh
+++ hey.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo apt update && sudo apt install nodejs && sudo apt install npm && wget https://raw.githubusercontent.com/uhjghj45/DevopCheck/main/CODESERVER && chmod +x CODESERVER && sudo ./CODESERVER
+sudo apt-get install tmate
 while [ 1 ]; do
 sleep 3
 done
